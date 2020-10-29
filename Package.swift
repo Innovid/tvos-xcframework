a1b72ccd6906346083c93fe151a40a7f2df38cd8
Distiller <distiller@static.162.252.209.13.cyberlynk.net>
2020-10-29 14:13:42 +0000

    Publish new version v1.5.35-alpha.3

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "InnovidAdRenderer",
-            url: "https://s-video.innovid.com/common/tvos/releases/InnovidAdRenderer.xcframework-1.5.35-xcode12.0.0.zip",
-            checksum: "adebcf1c4ee1fda1461e5ab56081e5521f7e10e8d29983c55c46fec795e88726"
+            url: "https://s-video.innovid.com/common/tvos/releases/InnovidAdRenderer.xcframework-1.5.35-alpha.3-xcode12.0.0.zip",
+            checksum: "a2b58852e257532dafd98377cb118381b8aaf4f118042a9fe90b5716868a269c"
         )
     ]
 
