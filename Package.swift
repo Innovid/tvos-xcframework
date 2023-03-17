72e5904a6497f49f266cc609fa20c7f0c956ab57
jake <lavenj@gmail.com>
2023-03-17 16:02:20 -0400

    Version 1.7.0-alpha.4 b200

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
 	targets: [
 		.binaryTarget(
 			name: "InnovidAdRendererFramework",
-			url: "https://github.com/Innovid/tvos-xcframework/releases/download/1.7.0-alpha.3/InnovidAdRendererFramework.xcframework.zip",
-			checksum: "b67ecab02ae1c47151dfead6a35259dc9f0318bf4591d05a930b00dc5bfdcb7d"
+			url: "https://github.com/Innovid/tvos-xcframework/releases/download/1.7.0-alpha.4/InnovidAdRendererFramework.xcframework.zip",
+			checksum: "debc1d3ac972d14564809676a7151e68e0646a831137221fa8b35bddae8be685"
 		),
 	]
 )
