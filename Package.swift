a402465acf707544b82dc90877da9b7d2b139d65
CTV Automation Test agent <ctvautomation@CTVs-MacBook-Pro.local>
2025-03-18 10:12:10 +0200

    Version 1.7.19 b19

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
 	targets: [
 		.binaryTarget(
 			name: "InnovidAdRendererFramework",
-			url: "https://github.com/Innovid/tvos-xcframework/releases/download/1.7.18/InnovidAdRendererFramework.xcframework.zip",
-			checksum: "9a3257bef0c64425551f1d8be4a1d44473b354a5081c6b9678ccc874a903bfa7"
+			url: "https://github.com/Innovid/tvos-xcframework/releases/download/1.7.19/InnovidAdRendererFramework.xcframework.zip",
+			checksum: "4f0d5f99d333fbc73b7dc6e1e9fdeb5f7476603a84dc10bf58c6c323abf2a46a"
 		),
 	]
 )
